3cfb89f0caba9da75ce7afbe717ed71a17c47e8f
hummus-erectus <robert.samuel.grayson@gmail.com>
2023-03-18 15:48:03 +0900

    Create close ticket functionality

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -40,13 +40,28 @@ export const getTickets = createAsyncThunk(
     }
 )
 
-// Get user icket
+// Get user ticket
 export const getTicket = createAsyncThunk(
     'tickets/get',
     async (ticketId, thunkAPI) => {
         try{
             const token = thunkAPI.getState().auth.user.token
             return await ticketService.getTicket(ticketId, token)
+        } catch (error) {
+            const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+
+            return thunkAPI.rejectWithValue(message)
+        }
+    }
+)
+
+// Close ticket
+export const closeTicket = createAsyncThunk(
+    'tickets/close',
+    async (ticketId, thunkAPI) => {
+        try{
+            const token = thunkAPI.getState().auth.user.token
+            return await ticketService.closeTicket(ticketId, token)
         } catch (error) {
             const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
 
@@ -100,6 +115,14 @@ export const ticketSlice = createSlice({
                 state.isLoading = false
                 state.isError = true
                 state.message = action.payload
+            })
+            .addCase(closeTicket.fulfilled, (state, action) => {
+                state.isLoading = false
+                state.tickets.map((ticket) => ticket._id === action.payload._id ?
+                    (ticket.status = 'closed')
+                    :
+                    ticket
+                )
             })
     }
 })
